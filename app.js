484f08b4ff8e9a0035a55075c6fc0a89bf001fdc
Rayan84 <rayan_84@live.com>
2021-09-13 15:19:35 +0200

    Add JavaScript code

---
+++ app.js
@@ -0,0 +1,34 @@
+// var book = function(title, author){
+//   title = title;
+//   author = author;
+// }
+
+// function addNewBook() { 
+//     var title = document.getElementById('title').value;
+//     var author = document.getElementById('author').value;
+//     var booksList = document.getElementById('books-list');
+    
+//     books.title.push(title);
+//     books.author.push(author);
+//     booksList.innerHTML = books
+// }
+
+
+
+class { constructor (title, author) {
+     this.title = title
+     this.author = author }
+     } 
+
+class UI { static displayBooks()
+    edBooks = [
+         { title: 'Book One', author: 'Denzel Nkomo', }, 
+         { title: 'Book 2', author: 'Rayan Shamoon', } ] 
+        }
+     }  { 
+         const StoredBooks = [
+            { title: 'Book One', author: 'Denzel Nkomo', },
+            { title: 'Book 2', author: 'Rayan Shamoon', }
+         ] 
+        } 
+    } 
