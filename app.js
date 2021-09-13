c2da44be6e9c80d60ca1c4aa9fc972421cac5ec9
unknown <djpants90@yahoo.com>
2021-09-13 17:21:18 +0200

    Run npx elsint and npx stylelint

--- app.js
+++ app.js
@@ -1,65 +1,41 @@
-var books = [];
+const books = [];
 
-var form = document.getElementById("book-form");
+const form = document.getElementById('book-form');
 function handleForm(event) {
-     event.preventDefault();
-     } 
-    form.addEventListener('submit', handleForm);
-
-
-
-function addNewBook() { 
-    var title = document.getElementById('title').value;
-    var author = document.getElementById('author').value;
-
-    books.push([title, author]);
-    // console.log(books);
-    // console.log(books.length);
-    renderBookList();
+  event.preventDefault();
 }
+form.addEventListener('submit', handleForm);
 
 function renderBookList() {
-    var booksListContainer = document.getElementById('book-list-container');
+  const booksListContainer = document.getElementById('book-list-container');
 
-        var p = document.createElement('P');
-        var pText = document.createTextNode(books[books.length - 1]);
-        var removeButton = document.createElement('Button');
-        var buttonText = document.createTextNode('Remove');
-        var line = document.createElement('Hr');
-        var totalBooks = books.length;
+  const p = document.createElement('P');
+  const pText = document.createTextNode(books[books.length - 1]);
+  const removeButton = document.createElement('Button');
+  const buttonText = document.createTextNode('Remove');
+  const line = document.createElement('Hr');
+  const totalBooks = books.length;
 
-     
-        p.appendChild(pText);
-        booksListContainer.appendChild(p);
-        removeButton.appendChild(buttonText);
-        removeButton.setAttribute('onclick', 'removeBook('+[totalBooks-1]+')');
-        booksListContainer.appendChild(removeButton);
-        booksListContainer.appendChild(line);
-
+  p.appendChild(pText);
+  booksListContainer.appendChild(p);
+  removeButton.appendChild(buttonText);
+  removeButton.setAttribute('onclick', `removeBook(${[totalBooks - 1]})`);
+  booksListContainer.appendChild(removeButton);
+  booksListContainer.appendChild(line);
 }
 
-function removeBook(num){
-    var removedParagraph = document.querySelectorAll('P');
-    var removedButton = document.querySelectorAll('Button');
-    removedButton[num].remove();
-    removedParagraph[num].remove();
-    removedParagraph.remove();
-    books.splice(num);
-    console.log(books);
+function addNewBook() {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+
+  books.push([title, author]);
+  renderBookList();
 }
 
-// function checkBookList() {
-    
-//     var booksListContainer = document.getElementById('book-list-container');
-//     if (books.length == 0) {
-//         booksListContainer.style.display = 'none';
-//         console.log('If');
-//     } else {
-//         booksListContainer.style.display = 'block';
-//         console.log('Else');
-//     }
-// }
-
-//checkBookList();
-
-
+function removeBook(num) {
+  const removedParagraph = document.querySelectorAll('P');
+  const removedButton = document.querySelectorAll('Button');
+  removedButton[num].remove();
+  removedParagraph[num].remove();
+  books.splice(num);
+}
