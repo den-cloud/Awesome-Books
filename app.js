75e451584fe4238c4def26433a093059911daa0f
unknown <djpants90@yahoo.com>
2021-09-14 13:33:06 +0200

    Add new JS code

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-var books = [];
+const books = [];
 
-var form = document.getElementById("book-form");
+const form = document.getElementById('book-form');
 function handleForm(event) {
-     event.preventDefault();
-     } 
-    form.addEventListener('submit', handleForm);
-
-
-
-addNewBook = () => { 
-    var title = document.getElementById('title').value;
-    var author = document.getElementById('author').value;
-    books.push([title, author]);
-    renderBookList();
+  event.preventDefault();
 }
+form.addEventListener('submit', handleForm);
+
+addNewBook = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  books.push([title, author]);
+  renderBookList();
+};
 
 renderBookList = () => {
-        var booksListContainer = document.getElementById('book-list-container');
-        var p = document.createElement('P');
-        var pText = document.createTextNode(books[books.length - 1]);
-        var removeButton = document.createElement('Button');
-        var buttonText = document.createTextNode('Remove');
-        var line = document.createElement('Hr');
-        var totalBooks = books.length;
+  const booksListContainer = document.getElementById('book-list-container');
+  const p = document.createElement('P');
+  const pText = document.createTextNode(books[books.length - 1]);
+  const removeButton = document.createElement('Button');
+  const buttonText = document.createTextNode('Remove');
+  const line = document.createElement('Hr');
+  const totalBooks = books.length;
 
-        p.appendChild(pText);
-        booksListContainer.appendChild(p);
-        removeButton.appendChild(buttonText);
-        removeButton.setAttribute('onclick', 'removeBook('+[totalBooks-1]+')');
-        booksListContainer.appendChild(removeButton);
-        booksListContainer.appendChild(line);
-}
+  p.appendChild(pText);
+  booksListContainer.appendChild(p);
+  removeButton.appendChild(buttonText);
+  removeButton.setAttribute('onclick', `removeBook(${[totalBooks - 1]})`);
+  booksListContainer.appendChild(removeButton);
+  booksListContainer.appendChild(line);
+};
 
 removeBook = (num) => {
-    var removedParagraph = document.querySelectorAll('P');
-    var removedButton = document.querySelectorAll('Button');
-    removedButton[num].remove();
-    removedParagraph[num].remove();
-    books.splice(num, 1);
-}
-
+  const removedParagraph = document.querySelectorAll('P');
+  const removedButton = document.querySelectorAll('Button');
+  removedButton[num].remove();
+  removedParagraph[num].remove();
+  books.splice(num, 1);
+};
